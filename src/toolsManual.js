6bb0a49777518a84319194b26c67d444556b51e1
杨斌 <yangb@2345.com>
2019-07-22 17:11:42 +0800

    Update toolsManual.js

--- src/toolsManual.js
+++ src/toolsManual.js
@@ -78,7 +78,7 @@ Tools.install = function(Vue) {
   Vue.prototype.$needRefreshData = needRefreshData
 
   // 暴露lodash
-  Vue.prototype.$lodash = lodash
+  window.$lodash = lodash
 
   window.setupWebViewJavascriptBridge = setupWebViewJavascriptBridge
 }
