105e8c3ef3e4964f9701781060e68f92b16b05a5
胡雷 <hulei@2345.com>
2019-09-02 15:44:14 +0800

    opt:去掉fetch

--- src/xhrHook.js
+++ src/xhrHook.js
@@ -76,47 +76,6 @@ let XhrHook = {
       }
       return _originSend.apply(this, arguments)
     }
-
-    if (window.fetch) {
-      let _origin_fetch = window.fetch
-      window.fetch = function() {
-        let startTime = Date.now()
-        let args = [].slice.call(arguments)
-
-        let fetchInput = args[0]
-        let method = 'GET'
-        let url
-
-        if (typeof fetchInput === 'string') {
-          url = fetchInput
-        } else if ('Request' in window && fetchInput instanceof window.Request) {
-          url = fetchInput.url
-          if (fetchInput.method) {
-            method = fetchInput.method
-          }
-        } else {
-          url = '' + fetchInput
-        }
-
-        if (args[1] && args[1].method) {
-          method = args[1].method
-        }
-
-        let fetchData = {
-          method: method,
-          url: url,
-          status: null
-        }
-
-        return _origin_fetch.apply(this, args).then(function(response) {
-          fetchData.status = response.status
-          fetchData.type = 'fetch'
-          fetchData.duration = Date.now() - startTime
-          cb(fetchData)
-          return response
-        })
-      }
-    }
   }
 }
 
